edf8707cd85a9f4f90f9e61b3ca775321de6ce47
youji44 <ansonmaster810@gmail.com>
2025-04-19 19:51:37 +0800

    11

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 interface User {
